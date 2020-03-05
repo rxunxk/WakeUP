26b6d0661b628517aea265fb9e5c26447cd8569d
Raunak <pandeyraunak64@gmail.com>
2020-03-05 14:10:17 +0530

    Alarm Receiver now calls mode specific Intents

--- app/src/main/java/com/raunak/alarmdemo4/Recievers/AlarmReceiver.java
+++ app/src/main/java/com/raunak/alarmdemo4/Recievers/AlarmReceiver.java
@@ -3,6 +3,9 @@ package com.raunak.alarmdemo4.Recievers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+
+import com.raunak.alarmdemo4.Activities.GameChaseTheBall;
 import com.raunak.alarmdemo4.Activities.ScreenShake;
 import com.raunak.alarmdemo4.Activities.SnoozeActivity;
 
@@ -13,14 +16,22 @@ public class AlarmReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         mode = intent.getStringExtra("mode");
 
+        Log.d("Mode : ",""+mode);
+
         if (mode.equals("quick")){
             Intent intentQuick = new Intent(context, SnoozeActivity.class);
             intentQuick.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(intentQuick);
-        }else {
+        }else if(mode.equals("E")){
             Intent intentSmart = new Intent(context, ScreenShake.class);
             intentSmart.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(intentSmart);
+        }else if (mode.equals("R")){
+            Intent gameIntent = new Intent(context, GameChaseTheBall.class);
+            gameIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(gameIntent);
+        }else if (mode.equals("D")){
+
         }
     }
 }
